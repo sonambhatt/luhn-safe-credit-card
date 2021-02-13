295e99565c41482fae649d999f6388d5bc86c200
Sonam <sonambhatt112@gmail.com>
2021-02-13 12:08:14 +0100

    [LUHN-000] Sets up Spring boot
    Sets up skeleton for Spring boot application
    Adds h2, spring-web and lombok dependencies

---
+++ src/main/java/com/publicisSapient/luhnSafeCard/LuhnSafeCardApplication.java
@@ -0,0 +1,13 @@
+package com.publicisSapient.luhnSafeCard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuhnSafeCardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuhnSafeCardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/publicisSapient/luhnSafeCard/LuhnSafeCardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.publicisSapient.luhnSafeCard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuhnSafeCardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
